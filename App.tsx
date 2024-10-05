802d4b0d169d813df47009a2896aab005a8d8641
RPM Dev <rpmdev@Ruans-MacBook-Air.local>
2024-10-05 05:28:55 -0300

    Revert "chore"
    This reverts commit e39a206c74ce07596bea014e80f5b93148121e8c.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
-import { StatusBar } from "react-native";
-import { Home } from "./src/screens/Home";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <StatusBar />
-      <Home />
-    </>
-  )
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
